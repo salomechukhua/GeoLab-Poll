93b80389d903c0e37148b3b47716c6a2ef6b6e85
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-04-01 19:46:06 +0400

    ძიება

--- app/Http/Controllers/Admin/SearchController.php
+++ app/Http/Controllers/Admin/SearchController.php
@@ -10,7 +10,13 @@ class SearchController extends Controller
 {
     public function index(Request $request)
     {
-        $question = Question::where('question', 'like', '%'.$request->results.'%')->get(); 
+        $question = 
+        Question::where('question', 'like', '%'.$request->results.'%')
+        ->orWhere('subject', 'like', '%'.$request->results.'%')
+        ->orWhere('type', 'like', '%'.$request->results.'%')
+        ->orWhere('value', 'like', '%'.$request->results.'%')
+        ->get(); 
+
         if(count($question) > 0){
         	return view('admin.questions.index', ['question'=>$question]);
         } else {
