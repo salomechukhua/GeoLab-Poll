39674c36e6f8cdf8a39aae1fc839f1083319ae0e
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-04-01 21:08:07 +0400

    მონაცემთა ბაზა

--- GeoLab-Poll.sql
+++ GeoLab-Poll.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2018 at 05:44 PM
+-- Generation Time: Apr 01, 2018 at 07:06 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -39,10 +39,10 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2018_03_27_120231_create_sessions_table', 2),
-(4, '2018_03_18_092138_create_questions_table', 3);
+(9, '2014_10_12_000000_create_users_table', 1),
+(10, '2014_10_12_100000_create_password_resets_table', 1),
+(11, '2018_03_18_092138_create_questions_table', 1),
+(12, '2018_03_27_120231_create_sessions_table', 1);
 
 -- --------------------------------------------------------
 
@@ -67,6 +67,7 @@ CREATE TABLE `questions` (
   `question` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `subject` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -75,8 +76,12 @@ CREATE TABLE `questions` (
 -- Dumping data for table `questions`
 --
 
-INSERT INTO `questions` (`id`, `question`, `subject`, `type`, `created_at`, `updated_at`) VALUES
-(1, 'დიდხანს შემიძლია თავსატეხების, ამოცანების ამოხსნა.', 'პროგრამირება', 'შესავალი', '2018-03-27 08:30:32', '2018-03-27 08:30:32');
+INSERT INTO `questions` (`id`, `question`, `subject`, `type`, `value`, `created_at`, `updated_at`) VALUES
+(1, 'დიდხანს შემიძლია თავსატეხების, ამოცანების ამოხსნა.', 'პროგრამირება', 'შესავალი', 1, '2018-04-01 09:31:54', '2018-04-01 09:42:05'),
+(4, 'თვლიან, რომ მდიდარი წარმოსახვა მაქვს.', 'დიზაინი', 'შესავალი', 1, '2018-04-01 12:22:27', '2018-04-01 12:22:27'),
+(5, 'ხალისით და დიდხანს შემიძლია რაღაცის გამოთვლა, დახაზვა.', 'პროგრამირება', 'შესავალი', 1, '2018-04-01 12:23:00', '2018-04-01 12:23:00'),
+(6, 'თვლიან, რომ შემოქმედებითი უნარები მაქვს.', 'დიზაინი', 'შესავალი', 1, '2018-04-01 12:23:26', '2018-04-01 12:23:26'),
+(7, 'მაინტერესებს სამეცნიერო-პოპულარული ლიტერატურა.', 'პროგრამირება', 'შესავალი', 1, '2018-04-01 12:24:01', '2018-04-01 12:24:01');
 
 -- --------------------------------------------------------
 
@@ -98,7 +103,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('bbUkCwxBtw44aBvDPZHAT2U0Sr1oyddBzZrQ5PxF', 1, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiNjlNRndQZHZIeVdUUXlON3kzYUNzOWdTbFBTYnJlQXdMT2c3N0RXTCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NjI6Imh0dHA6Ly9sb2NhbGhvc3QvR2VvTGFiLVBvbGwvcHVibGljL2FkbWluL3BhZ2VzL3Byb2ZpbGUvMS9lZGl0Ijt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1522338208);
+('7asZ9kMCaL9pJfkO2gFxSqG0kaTi4uSt56kOOkQB', 4, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiZGtFR05PYjlnTzNZU3o2c2RYc1U5ZnZRbW1MNjNMaXVaVUpDcVNXdCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NTU6Imh0dHA6Ly9sb2NhbGhvc3QvR2VvTGFiLVBvbGwvcHVibGljL2FkbWluL3BhZ2VzL3Byb2ZpbGUiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTo0O30=', 1522599532),
+('dIpWTio613F3kCpHZuPugzAJg7PXsUkeJjZrUZyv', 4, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiUURLQk55TklkbFBlVWV0M213bnFQRlB3ZHVrdjlIUXRXbXBOQW9nbiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzU6Imh0dHA6Ly9sb2NhbGhvc3QvR2VvTGFiLVBvbGwvcHVibGljIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6NDt9', 1522601931);
 
 -- --------------------------------------------------------
 
@@ -110,8 +116,9 @@ CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` text COLLATE utf8mb4_unicode_ci,
+  `image` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `repassword` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -121,8 +128,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `image`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@polls.ge', 'https://is3-ssl.mzstatic.com/image/thumb/Purple4/v4/f9/ee/7a/f9ee7a6f-355d-eef7-c622-aacb555d5046/mzl.ygcffwfd.jpg/1200x630bb.jpg', '$2y$10$FDEGLfGzhzoz0m7vBZBlROmWkA9/iet.QRC8/tUf0PIDkiNTYtAuq', NULL, NULL, NULL);
+INSERT INTO `users` (`id`, `name`, `email`, `image`, `password`, `repassword`, `remember_token`, `created_at`, `updated_at`) VALUES
+(4, 'admin', 'admin@polls.ge', 'owll.jpg', '$2y$10$EvOlKVKEIOBu1FuG/pKGceCGWrXvg9q83iAvCjgfqiH7SYt4d7EVO', '$2y$10$Dzj7cGQ..bP35.3PQTr7eekRi3FeD6mavouL7ELtHWLy5IZUnFH4S', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -167,19 +174,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
